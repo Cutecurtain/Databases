a69ad98a5ea0f1c91a7cc1904f9a19d441e328d9
cutecurtain <aron.sandstedt@live.se>
2018-01-24 13:40:51 +0100

    fix misstakes

--- tables.sql
+++ tables.sql
@@ -6,52 +6,68 @@ CREATE TABLE Program (
 );
 
 --Department(_name_, abbreviation)
+--	UNIQUE abbreviation
 CREATE TABLE Department (
 	name			TEXT		PRIMARY KEY,
-	abbreviation	TEXT		NOT NULL
+	abbreviation	TEXT		UNIQUE		NOT NULL
 );
 
 --Hosts(_department_, _program_)
+--	department -> Department.name
+--	program -> Program.name
 CREATE TABLE Hosts (
-	department		Department,
-	program			Program,
+	department		TEXT,
+	program			TEXT,
 	PRIMARY KEY(department, program)
 );
 
 --Student(_ssn_, name, login, program)
+--	program -> Program.name
+--	UNIQUE login
+--	UNIQUE (ssn, program)
 CREATE TABLE Student (
 	ssn				TEXT		PRIMARY KEY,
 	name			TEXT		NOT NULL,
 	login			TEXT		NOT NULL,
-	program			Program		NOT NULL
+	program			TEXT		NOT NULL,
+	UNIQUE(login, ssn, program)
 );
 
 --Branch(_name_, _program_)
+--	program -> Program.name
 CREATE TABLE Branch (
 	name			TEXT,
-	program			Program,
+	program			TEXT,
 	PRIMARY KEY(name, program)
 );
 
 --BelongsTo(_student_, branch, program)
-CREATE TABLE BelongsTo (
-	student			Student		PRIMARY KEY,
-	branch			Branch		NOT NULL,
-	program			Program		NOT NULL
+--	student -> Student.ssn
+--	(student, program) -> Student.(ssn, program)
+--	(branch, program) -> Branch.(name, program)
+CREATE TABLE BelongsTo (-------------------------------------------------
+	student			TEXT		PRIMARY KEY,
+	branch			TEXT		NOT NULL,
+	program			TEXT		NOT NULL,
+	FOREIGN KEY(student)			REFERENCES Student(ssn),
+	FOREIGN KEY(branch, program)	REFERENCES Branch(name, program)
 );
 
 --Course(_code_, name, credits, department)
+--	department -> Department.name
 CREATE TABLE Course (
 	code			CHAR(6)		PRIMARY KEY,
 	name			TEXT		NOT NULL,
 	credits			TEXT		NOT NULL,
-	department		Department	NOT NULL
+	department		TEXT		NOT NULL
 );
 
 --Prerequisite(_course_, _prerequisite_)
+--	course -> Course.code
+--	prerequisite -> Course.code
 CREATE TABLE Prerequisite (
-	course			Course,
-	prerequisite	TEXT,
+	course			CHAR(6),
+	prerequisite	CHAR(6),
 	PRIMARY KEY(course, prerequisite)
 );
 
@@ -62,60 +78,79 @@ CREATE TABLE Classification (
 
 
 --Classified(_course_, _classification_)
+--	course -> Course.code
+--	classification -> Classification.name
 CREATE TABLE Classified (
-	course			Course,
-	classification	Classification,
+	course			CHAR(6),
+	classification	TEXT,
 	PRIMARY KEY(course, classification)
 );
 
 --MandatoryProgram(_course_, _program_)
+--	course -> Course.code
+--	program -> Program.name
 CREATE TABLE MandatoryProgram (
-	course			Course,
-	program			Program,
+	course			CHAR(6),
+	program			TEXT,
 	PRIMARY KEY(course, program)
 );
 
 --MandatoryBranch(_course_, _branch_, _program_)
-CREATE TABLE MandatoryBranch (
-	course			Course,
-	branch			Branch,
-	program			Program,
-	PRIMARY KEY(course, branch, program)
+--	course -> Course.code
+--	(branch, program) -> Branch.(name, program)
+CREATE TABLE MandatoryBranch (-------------------------------------------
+	course			CHAR(6),
+	branch			TEXT,
+	program			TEXT,
+	PRIMARY KEY(course, branch, program),
+	FOREIGN KEY(branch, program) REFERENCES Branch(name, program)
 );
 
 --RecommendedBranch(_course_, _branch_, _program_)
-CREATE TABLE RecommendedBranch (
-	course			Course,
-	branch			Branch,
-	program			Program,
-	PRIMARY KEY(course, branch, program)
+--	course -> Course.code
+--	(branch, program) -> Branch.(name, program)
+CREATE TABLE RecommendedBranch (-----------------------------------------
+	course			CHAR(6),
+	branch			TEXT,
+	program			TEXT,
+	PRIMARY KEY(course, branch, program),
+	FOREIGN KEY(branch, program) REFERENCES Branch(name, program)
 );
 
 --Registered(_student_, _course_)
+--	student -> Student.ssn
+--	course -> Course.code
 CREATE TABLE Registered (
-	student			Student,
-	course			Course,
+	student			TEXT,
+	course			CHAR(6),
 	PRIMARY KEY(student, course)
 );
 
 --Taken(_student_, _course_, grade)
+--	student -> Student.ssn
+--	course -> Course.code
 CREATE TABLE Taken (
-	student			Student,
-	course			Course,
+	student			TEXT,
+	course			CHAR(6),
 	grade			INT			NOT NULL,
 	PRIMARY KEY(student, course)
 );
 
 --LimitedCourse(_code_, seats)
+--	code -> Course.code
 CREATE TABLE LimitedCourse (
 	code			CHAR(6)		PRIMARY KEY,
 	seats			INT			NOT NULL
 );
 
 --WaitingList(_student_, _course_, position)
+--	student -> Student.ssn
+--	course -> LimitedCourse.code
+--	UNIQUE (position, course)
 CREATE TABLE WaitingList (
-	student			Student,
-	course			Course,
+	student			TEXT,
+	course			CHAR(6),
 	position		TEXT		NOT NULL,
-	PRIMARY KEY(student, course)
+	PRIMARY KEY(student, course),
+	UNIQUE(position, course)
 );
